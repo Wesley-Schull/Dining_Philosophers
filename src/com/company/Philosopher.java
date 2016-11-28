f5abd609300ba716215acf65b2969912ad334df7
Wesley Schull <wcschull@bsu.edu>
2016-11-28 07:00:50 -0500

    Start all threads simultaneously

--- src/com/company/Philosopher.java
+++ src/com/company/Philosopher.java
@@ -3,12 +3,10 @@ package com.company;
 public class Philosopher implements Runnable {
     int id;
     public void run(){
-//        while(miscsubs.TotalEats < miscsubs.MAX_EATS){
-            miscsubs.RandomDelay();
-            miscsubs.StartEating(id);
-            miscsubs.RandomDelay();
-            miscsubs.DoneEating(id);
-//        }
+        miscsubs.RandomDelay();
+        miscsubs.StartEating(id);
+        miscsubs.RandomDelay();
+        miscsubs.DoneEating(id);
     }
     public Philosopher(int idNumber){
         this.id = idNumber;
--- src/com/company/dining.java
+++ src/com/company/dining.java
@@ -2,21 +2,30 @@ package com.company;
 
 class dining
 {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) throws InterruptedException {
         System.out.println("Starting the Dining Philosophers Simulation\n");
         miscsubs.InitializeChecking();
         // Your code here...
+        Thread mainThread = new Thread();
+        mainThread.start();
         Philosopher[] P = new Philosopher[miscsubs.NUMBER_PHILOSOPHERS];
+        Thread[] threads = new Thread[miscsubs.NUMBER_PHILOSOPHERS];
 //        miscsubs.StartEating(3);
         for(int i = 0; i < P.length; i++) {
             P[i] = new Philosopher(i);
         }
-        P[0].run();
-        P[1].run();
-        P[2].run();
-        P[3].run();
-        P[4].run();
+//        P[0].run();
+//        P[1].run();
+//        P[2].run();
+//        P[3].run();
+//        P[4].run();
+        for(int i = 0; i < P.length; i++){
+            threads[i] = new Thread(P[i]);
+        }
+        for(Thread thread: threads) {
+            thread.join();
+            thread.start();
+        }
 
 
 //        while(miscsubs.TotalEats < miscsubs.MAX_EATS){
@@ -26,6 +35,7 @@ class dining
 //        }
 //        miscsubs.DoneEating(3);
 //        new Philosopher().run();
+
         // End of your code
         miscsubs.LogResults();
     }
